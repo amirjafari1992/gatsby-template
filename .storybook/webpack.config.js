2e533e02f370d4b60b01fce355a11f189572e0db
amir <amir@mindly.ee>
2020-05-05 11:53:15 +0300

    update dependencies

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,30 @@
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            // Creates `style` nodes from JS strings
+            'style-loader',
+            // Translates CSS into CommonJS
+            'css-loader',
+            // Compiles Sass to CSS
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.svg$/,
+          use: [
+            {
+              loader: 'svg-url-loader',
+              options: {
+                limit: 10000,
+              },
+            },
+          ],
+        },
+      ],
+    },
+  };
+  const webpack = require('webpack');
+
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,8 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
+import "../styles/general.scss"
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
